19289dc47d80aecdb855aabe01908e66ee593fd8
Liam Snodgrass <dropthepoptart13@gmail.com>
2017-10-26 12:21:43 -0400

    Update index.php

--- music/index.php
+++ music/index.php
@@ -10,13 +10,8 @@
 <center>
 
 <div class="center">
-<h2>How to get the ID of a Youtube Video</h2>
-Step 1: Find the YouTube Link (either via YouTube site or via Google Search.)
-<p>Step 2: Copy the part after the "watch?v=".</p>
-<p>Step 3: Paste the ID and click submit.</p>
-<p>Note! If the video/music does not play, it is most likely over the file size limit.</p>
 <form action="loading.php" method="get">
-ID: <input type="text" name="id">
+Link to Video: <input type="text" name="id">
 <p><input type="Submit"></p>
 </form>
 </div>
