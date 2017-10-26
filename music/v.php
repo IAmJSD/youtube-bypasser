38411b4cadca5c4599bd6f75953aa835e27cac4b
Liam Snodgrass <dropthepoptart13@gmail.com>
2017-10-26 12:20:44 -0400

    Allowed to paste YT id

--- music/v.php
+++ music/v.php
@@ -6,12 +6,14 @@
 <body>
 
 <?php 
-exec('youtube-dl -k --download-archive download.txt -x --audio-format mp3 --output "/var/www/html/m/%(id)s.%(ext)s" https://youtube.com/watch?v=' . $_GET["id"] . '--max-filesize 100MB')
+$yt = $_GET["id"];
+$id = str_replace("https://www.youtube.com/watch?v=","\n",$yt);
+exec('youtube-dl -k --download-archive download.txt -x --audio-format mp3 --output "m/%(id)s.%(ext)s" ' . $_GET["id"] . '--max-filesize 100MB')
 ?>
 
 <div class="center">
 <audio controls autoplay>
-	<source src="/m/<?php echo $_GET["id"] ?>.mp3">
+	<source src="/m/<?php echo $id ?>.mp3">
 </audio>
 </div>
 </body>
