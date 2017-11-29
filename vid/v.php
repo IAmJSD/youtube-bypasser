5a741835de56f234c2d459e1523a91c28d9259c9
JakeMakesStuff <jake@gealer.email>
2017-11-29 21:32:17 +0000

    PLEEEEEEEEEEEEEEEEEEEEASE sanitize your PHP

--- vid/v.php
+++ vid/v.php
@@ -7,9 +7,16 @@
 
 
 <?php 
-$yt = $_GET["id"];
+function CleanSTR($str) {
+	$disallowedchars = ["\0", "\n", "\r", "*", "'", '"'];
+	foreach($disallowedchars as $char) {
+	    $str = str_replace($char, "X", $str);
+	}
+	return $str;
+}
+$yt = CleanSTR($_GET["id"]);
 $id = str_replace("https://www.youtube.com/watch?v=","\n",$yt);
-exec('youtube-dl -f 22 --download-archive download.txt --output "v/%(id)s.%(ext)s" ' . $_GET["id"] . '--max-filesize 600MB')
+exec('youtube-dl -f 22 --download-archive download.txt --output "v/%(id)s.%(ext)s" ' . $yt . '--max-filesize 600MB')
 ?>
 
 <div class="center">
